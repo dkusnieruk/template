cf96d56ab6475d35434c44cc427b2561f4308e0b
dkusnieruk <dariusz.kusnieruk@wp.pl>
2023-01-10 21:57:12 +0100

    update task
    load more gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
+import { useState, useEffect } from "react";
+
+
+function App  ()  {
+
+  const [count, setCount] = useState(0);
+  const [student ,setStudent] = useState({name:'John', age:25})
+
+  const incrementCount =() =>{
+    setCount(count +1);
+  }
+
+  const updateStudent =() =>{
+    setStudent({
+      ...student,
+      name:'Andrzej'
+
+    })
+  }
+  useEffect(() =>{
+console.log('Hello from UE');
+document.title = `Count :${count}`
+
+  }, [count])
+
+  const [isTrue, setIsTrue] = useState(false)
+
+  const toggle =( ) =>{
+    setIsTrue(!isTrue);
+  }
+ 
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    className="App"
     >
-      React homework template
+<h2>Hooks - count = {count}</h2>
+<h2>Student : {student.name} age : {student.age}</h2>
+<h4>Is it true {isTrue ?"Yes" : "No" }</h4>    
+    <button onClick={incrementCount}>Count</button>
+    <button onClick={updateStudent}>updateStudent</button>
+    <button onClick={toggle}>Toggle</button>
     </div>
   );
 };
+
+export default App
